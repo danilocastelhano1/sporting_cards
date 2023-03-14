289657598dae8bdd0e3c91a2c223ed262953692f
Danilo Antonio Castelhano <danilocastelhano@hotmail.com>
2023-03-14 14:02:16 -0300

    fixing create and update serializer

--- cards/api/serializers.py
+++ cards/api/serializers.py
@@ -11,9 +11,34 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class CardSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
+    tags = serializers.ListSerializer(
+        child=serializers.CharField(allow_blank=False))  # TagSerializer(many=True, read_only=False)
 
     class Meta:
         model = Card
         fields = ("id", "texto", "data_criacao", "data_modificacao", "tags")
         read_only_fields = ("id", "data_criacao", "data_modificacao")
+
+    def create(self, validated_data):
+        tags = validated_data.pop("tags")
+
+        card = super(CardSerializer, self).create(validated_data)
+
+        for tag in tags:
+            tag_obj, created_tag = Tag.objects.get_or_create(name=tag, defaults={"name": tag})
+            card.tags.add(tag_obj)
+
+        return card
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop("tags")
+
+        card = super(CardSerializer, self).update(instance, validated_data)
+
+        card.tags.all().delete()
+
+        for tag in tags:
+            tag_obj, created_tag = Tag.objects.get_or_create(name=tag, defaults={"name": tag})
+            card.tags.add(tag_obj)
+
+        return card
